175e0232758c54b4908df50ab572f822958fb456
Matthaeus <matthaeustjia@gmail.com>
2021-05-30 03:31:37 +1000

    removed login form bar and text

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -3,10 +3,6 @@
     <v-layout align-center justify-center>
       <v-flex xs12 sm8 md4>
         <v-card class="elevation-12">
-          <v-toolbar dark color="primary">
-            <v-toolbar-title>Login form</v-toolbar-title>
-            <v-spacer></v-spacer>
-          </v-toolbar>
           <v-card-text>
             <v-form @submit.prevent="signIn">
               <v-text-field
